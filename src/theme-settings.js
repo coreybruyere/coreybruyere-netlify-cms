b8845d45d828a5c6e6ce4d734c0d7cbc6b2926de
Corey Bruyere <coreybruyere@ymail.com>
2018-05-21 21:35:36 -0700

    Add theme tokens JSON file

---
+++ src/theme-settings.js
@@ -0,0 +1,69 @@
+export default {
+  borders: {
+    widthThin: "1px",
+    widthThick: "2px",
+    widthThicker: "4px",
+    widthThickest: "8px",
+    radiusSmall: "0.125rem",
+    radiusDefault: "0.25rem",
+    radiusCircle: "50%"
+  },
+  breakpoints: {
+    min: "280px",
+    xsmall: "360px",
+    small: "480px",
+    medium: "504px",
+    large: "800px",
+    xlarge: "960px",
+    max: "1280px"
+  },
+  colors: {
+    primary: {
+      light: "#00CCB8",
+      base: "#009688",
+      dark: "#006B60"
+    },
+    secondary: {
+      light: "",
+      base: "",
+      dark: ""
+    },
+    neutral: {
+      lightest: "",
+      lighter: "",
+      light: "",
+      base: "",
+      dark: "",
+      darker: "",
+      darkest: ""
+    }
+  },
+  fonts: {
+    familySans: "'Source Sans Pro', sans-serif",
+    familySerif: "'Source Serif Pro', serif",
+    familyMonospace: "'Source Code Pro', monospace",
+    familySystem: "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif"
+  },
+  layers: {
+    modal: 9000,
+    backdrop: 8000,
+    overlay: 1000,
+    default: 1,
+    underlay: -1
+  },
+  sizes: {
+    xsmall: "0.25rem",
+    small: "0.75rem",
+    medium: "1.5rem",
+    large: "3rem",
+    xlarge: "4.5rem"
+  },
+  spaces: {
+    xsmall: "0.25rem",
+    small: "0.5rem",
+    medium: "1rem",
+    large: "1.5rem",
+    xlarge: "3rem",
+    xxlarge: "4.5rem"
+  }
+}
