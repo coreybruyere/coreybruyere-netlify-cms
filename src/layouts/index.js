7da4b14199adffbb9dec97c3421b3f4e7376731a
Corey Bruyere <coreybruyere@ymail.com>
2018-05-21 21:35:03 -0700

    Add initial global styles

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
+import { injectGlobal } from 'styled-components';
 
 import SiteHeader from '../components/SiteHeader';
 import './all.sass';
+
+injectGlobal`
+  html {
+    -webkit-font-smoothing: antialiased;
+    overflow-x: hidden;
+    overflow-y: scroll;
+    text-rendering: optimizeLegibility;
+    text-size-adjust: 100%;
+  }
+  body {
+    font-size: 1rem;
+    font-weight: 400;
+    line-height: 1.5;
+  }
+`
 
 const TemplateWrapper = ({ children }) => (
   <div>
